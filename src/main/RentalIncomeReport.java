e9d9acc6ed255a308d96f5d4b6691cd59694bffc
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-06-11 12:29:24 -0700

    Add specification file for the RentalIncomeReport class

--- src/main/RentalIncomeReport.java
+++ src/main/RentalIncomeReport.java
@@ -1,5 +1,17 @@
 package main;
 
-public class RentalIncomeReport {
+import java.util.List;
 
+public class RentalIncomeReport {
+	//TODO: columns and rows
+	private List<IncomeRecord> records;
+
+	
+	public RentalIncomeReport(List<IncomeRecord> records) {
+		this.records = records;
+	}
+	
+	public void recordIncomePayment(String rent) {
+		//TODO: method to record a rent record
+	}
 }

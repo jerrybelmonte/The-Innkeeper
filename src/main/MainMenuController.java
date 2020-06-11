7bcd3574baf806bfc62843a292478014841127c2
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-06-11 12:36:56 -0700

    Add operations for the MainMenuController class

--- src/main/MainMenuController.java
+++ src/main/MainMenuController.java
@@ -2,4 +2,38 @@ package main;
 
 public class MainMenuController {
 
+	public void inputTenant() {
+		//TODO: method to input a new tenant
+	}
+	
+	
+	public void printTenantList() {
+		//TODO: method to print tenant list
+	}
+	
+	
+	public void inputIncomeRecord() {
+		//TODO: method to input a rental record
+	}
+	
+	
+	public void printIncomeReport() {
+		//TODO: method to print the income report
+	}
+	
+	
+	public void inputExpenseRecord() {
+		//TODO: method to input an expense
+	}
+	
+	
+	public void printExpenseReport() {
+		//TODO: method to print the expense report
+	}
+	
+	
+	public void printAnnualSummary() {
+		//TODO: method to print the annual summary
+	}
+	
 }

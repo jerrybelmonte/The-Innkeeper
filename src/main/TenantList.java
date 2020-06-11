630bbf128f9b43450865e324f811e8a98cd881e4
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-06-11 12:25:19 -0700

    Add specification for the TenantList class

--- src/main/TenantList.java
+++ src/main/TenantList.java
@@ -1,5 +1,19 @@
 package main;
 
-public class TenantList {
+import java.util.List;
 
+public class TenantList {
+	//TODO: columns
+	private List<Tenant> tenants;
+
+	
+	public TenantList(List<Tenant> tenants) {
+		this.tenants = tenants;
+	}
+	
+	
+	public void displayTenants() {
+		//TODO: method to display the tenants
+	}
+	
 }

7f31ed3210ac1622be61a01f6628a944485e5997
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-06-11 16:32:08 -0700

    Add first iteration of the AddNewTenantTest

--- src/main/Tenant.java
+++ src/main/Tenant.java
@@ -1,28 +1,46 @@
 package main;
 
+/**
+ * A tenant that rents an apartment in John's apartment building.
+ * 
+ * @author Jerry Belmonte
+ */
 public class Tenant {
+	/** The name of the tenant. */
 	private String tenantName;
+	/** The tenant's apartment number. */
 	private int apartmentNumber;
 
+
+	/**
+	 * Normal constructor for the Tenant class.
+	 * 
+	 * @param tenantName	  Their full name.
+	 * @param apartmentNumber Their apartment number.
+	 */
 	public Tenant(String tenantName, int apartmentNumber) {
 		this.tenantName = tenantName;
 		this.apartmentNumber = apartmentNumber;
 	}
 
+
 	public String getTenantName() {
 		return tenantName;
 	}
+
 
 	public void setTenantName(String tenantName) {
 		this.tenantName = tenantName;
 	}
 
+
 	public int getApartmentNumber() {
 		return apartmentNumber;
 	}
+
 
 	public void setApartmentNumber(int apartmentNumber) {
 		this.apartmentNumber = apartmentNumber;
 	}
 
-}
+} // End of the Tenant class.
--- src/test/AddNewTenantTest.java
+++ src/test/AddNewTenantTest.java
@@ -8,47 +8,84 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import main.Tenant;
+
+/**
+ * Unit test for the Use case: Add New Tenant
+ * 
+ * @author Jerry Belmonte
+ */
 class AddNewTenantTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
 
+
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
 	}
+
 
 	@BeforeEach
 	void setUp() throws Exception {
 	}
 
+
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
+
 	@Test
-	void testTenant() {
-		fail("Not yet implemented"); // TODO
+	void testTenantConstructor() {
+		String theirName = "Some Name";
+		int theirApartment = 100;
+		Tenant firstTenant = new Tenant(theirName, theirApartment);
+		
+		assertNotNull(firstTenant);
 	}
+
 
 	@Test
 	void testGetTenantName() {
-		fail("Not yet implemented"); // TODO
+		String theirName = "Some Name";
+		int theirApartment = 100;
+		Tenant firstTenant = new Tenant(theirName, theirApartment);
+
+		assertTrue(firstTenant.getTenantName().equals(theirName));
 	}
+
 
 	@Test
 	void testSetTenantName() {
-		fail("Not yet implemented"); // TODO
+		String theirName = "Some Name";
+		int theirApartment = 100;
+		Tenant firstTenant = new Tenant(theirName, theirApartment);
+		firstTenant.setTenantName("Another Name");
+
+		assertTrue(firstTenant.getTenantName().equals("Another Name"));
 	}
+
 
 	@Test
 	void testGetApartmentNumber() {
-		fail("Not yet implemented"); // TODO
+		String theirName = "Some Name";
+		int theirApartment = 100;
+		Tenant firstTenant = new Tenant(theirName, theirApartment);
+
+		assertEquals(theirApartment, firstTenant.getApartmentNumber());
 	}
+
 
 	@Test
 	void testSetApartmentNumber() {
-		fail("Not yet implemented"); // TODO
+		String theirName = "Some Name";
+		int theirApartment = 100;
+		Tenant firstTenant = new Tenant(theirName, theirApartment);
+		firstTenant.setApartmentNumber(200);
+
+		assertEquals(200, firstTenant.getApartmentNumber());
 	}
 
-}
+} // End of the AddNewTenantTest class.

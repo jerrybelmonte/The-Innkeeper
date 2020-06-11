5a669e2c5e6fb857046627f31a3f8721d5db0dc8
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-06-11 15:36:02 -0700

    Change name of TenantTest to AddNewTenantTest

--- src/test/AddNewTenantTest.java
+++ src/test/AddNewTenantTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TenantTest {
+class AddNewTenantTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {

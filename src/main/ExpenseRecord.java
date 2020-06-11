5fb81b6a93d80d3d69fe17dc8d6a4fa4ad05078c
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-06-11 12:18:56 -0700

    Add attributes and methods for ExpenseRecord class

--- src/main/ExpenseRecord.java
+++ src/main/ExpenseRecord.java
@@ -1,5 +1,23 @@
 package main;
 
 public class ExpenseRecord {
-
+	private int month;
+	private int day;
+	private String category;
+	private float amount;
+	
+	
+	public ExpenseRecord(int month, int day, String category, float amount) {
+		this.month = month;
+		this.day = day;
+		this.category = category;
+		this.amount = amount;
+	}
+	
+	
+	public String recordExpense() {
+		//TODO: return an expense record
+		return null;
+	}
+	
 }

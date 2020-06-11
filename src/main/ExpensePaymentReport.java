78cb53da59e8c9ac379aa4a249f36645deba9ad1
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-06-11 12:23:01 -0700

    Add specifications for ExpensePaymentReport class

--- src/main/ExpensePaymentReport.java
+++ src/main/ExpensePaymentReport.java
@@ -1,5 +1,17 @@
 package main;
 
-public class ExpensePaymentReport {
+import java.util.List;
 
+public class ExpensePaymentReport {
+	//TODO: columns
+	private List<ExpenseRecord> records;
+
+	public ExpensePaymentReport(List<ExpenseRecord> records) {
+		this.records = records;
+	}
+	
+	public void recordExpensePayment(ExpenseRecord expense) {
+		//TODO: record the payment
+	}
+	
 }

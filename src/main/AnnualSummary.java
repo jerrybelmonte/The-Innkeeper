564dff23ce83e8f96bed1ca65d4307682c1d340d
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-06-11 12:32:23 -0700

    Add specification for AnnualSummary class

--- src/main/AnnualSummary.java
+++ src/main/AnnualSummary.java
@@ -2,4 +2,16 @@ package main;
 
 public class AnnualSummary {
 
+	public void displayIncomeReport() {
+		//TODO: method to display the year to date income report
+	}
+	
+	
+	public void displayExpenseReport() {
+		//TODO: method to display the year to date expenses
+	}
+	
+	public void displayBalance() {
+		//TODO: method to display the balance of + income - expense
+	}
 }

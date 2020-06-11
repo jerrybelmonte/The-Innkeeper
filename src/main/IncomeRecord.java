f1c42aa528fb1cda214c2a15c6362aa3370411a7
Jerry Belmonte <24368193+jerrybelmonte@users.noreply.github.com>
2020-06-11 12:27:16 -0700

    Add specification for the IncomeRecord class

--- src/main/IncomeRecord.java
+++ src/main/IncomeRecord.java
@@ -1,5 +1,21 @@
 package main;
 
 public class IncomeRecord {
-
+	private String name;
+	private float amount;
+	private int month;
+	
+	
+	public IncomeRecord(String name, float amount, int month) {
+		this.name = name;
+		this.amount = amount;
+		this.month = month;
+	}
+	
+	
+	public String recordRent() {
+		//TODO: return a rent record
+		return null;
+	}
+	
 }

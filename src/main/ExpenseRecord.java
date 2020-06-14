cb0e156c375e5c693c62d235678357007826e706
Howard Moon <mhw0619@gmail.com>
2020-06-14 16:05:13 -0700

    Update to ExpenseRecord and Report

--- src/main/ExpenseRecord.java
+++ src/main/ExpenseRecord.java
@@ -2,19 +2,24 @@ package main;
 
 // Howard
 // Create a record of expenses shown in month, day, amount, and category.
+// Added name to show which expense belongs to who.
 
 public class ExpenseRecord {
 	private int month;
 	private int day;
 	private String category;
 	private float amount;
+	private TenantList tenant = new TenantList();
+	private int apartmentN;
+	private String name;
 	
 	
-	public ExpenseRecord(int month, int day, String category, float amount) {
+	public ExpenseRecord(String name, int month, int day, String category, float amount) {
 		this.month = month;
 		this.day = day;
 		this.category = category;
 		this.amount = amount;
+		this.apartmentN = tenant.getApartmentNumber(name); // Tenant's name and apartment number.
 	}
 
 	public int getMonth() {
@@ -49,9 +54,18 @@ public class ExpenseRecord {
 		this.amount = amount;
 	}
 
+	public void setApartmentN(int apartmentN) {
+		this.apartmentN = apartmentN;
+	}
+
+	public int getApartmentN() {
+		return apartmentN;
+	}
+
 
 	public String recordExpense() {
-		return "Month: " + month + " Day: " + day + " Category: " + category + " Expense Amount: " + amount;
+		return "Apartment Number: " + apartmentN + " Month: " + month + " Day: " + day + " Category: "
+				+ category + " Expense Amount: " + amount;
 	}
 	
 }
